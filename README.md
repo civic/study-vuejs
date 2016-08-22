109d063c9a57acedf9e55164ab24c7a121394d27
Takashi Sasaki <tsskmail@gmail.com>
2016-08-23 00:51:16 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+webpack、gulpの復習とvue.js
+===============================
+
+webpack, gulpを使ってjsをビルドする基本的なJS開発をおさらいしてから、vue.jsのガイドをすすめる。
+
+<https://jp.vuejs.org/guide/installation.html>
+
+準備
+-----
+
+nodeをインストールしておく。
+
---
+++ workspace/helloworld/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var webpack = require("webpack-stream");
+var browserSync = require('browser-sync').create();
+
+
+//webpackでのビルド
+gulp.task('js', function(){
+  gulp.src('./app.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js',
+      },
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/
+          }
+        ]
+      }
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+//brwoser-syncの起動とファイルの監視
+gulp.task('dev', ["js"], function(){
+  browserSync.init({
+    server: {
+      baseDir: '.'
+    },
+    files: [
+      { match: ['app.js', '*.html'] }
+    ],
+    open: true
+  });
+  gulp.watch(['app.js', '*.html'], ['js']);
+});
+
+gulp.task('build', ['js']);
