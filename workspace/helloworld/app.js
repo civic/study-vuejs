597bc4ec54c6d00c1dda6601fb3e92ce3772eb10
Takashi Sasaki <tsskmail@gmail.com>
2016-08-23 23:51:36 +0900

    データ追記

--- workspace/helloworld/app.js
+++ workspace/helloworld/app.js
@@ -3,6 +3,11 @@ import Vue from 'vue'
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js!',
+    todos: [
+      {text: 'Learn Javascript'},
+      {text: 'Learn Vue.js'},
+      {text: 'Build Something Awesome'}
+    ]
   }
 });
