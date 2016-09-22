537ddaf17477ea3862c6d92610cdd7f51d67f03c
Takashi Sasaki <tsskmail@gmail.com>
2016-09-22 16:30:41 +0900

    計算機課題終了分コミット

--- workspace/calc-input/app.js
+++ workspace/calc-input/app.js
@@ -3,8 +3,35 @@ import Vue from 'vue'
 var vm = new Vue({
   el: '#app',
   data: {
+    a: 0,
+    b: 0,
+    op: null
+  },
+  computed: {
+    c: function(){
+      if (this.op === "+") {
+        return this.a + this.b;
+      } else if (this.op === "-") {
+        return this.a - this.b;
+      } else if (this.op === "*") {
+        return this.a * this.b;
+      } else if (this.op === "/") {
+        return this.a / this.b;
+      }
+      return null;
+
+    }
   },
   methods: {
+    onClickA: function(a){
+      this.a = a;
+    },
+    onClickB: function(b){
+      this.b = b;
+    },
+    onClickOp: function(op){
+      this.op = op;
+    }
   }
 });
 
